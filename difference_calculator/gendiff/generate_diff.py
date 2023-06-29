9a8d115563035681b699eff56da8ec9f563a2e0d
NevermoreKatana <crymorebch@gmail.com>
2023-06-29 12:53:24 +0300

    Add Parser, refactoring logic and add support yaml format file

--- difference_calculator/gendiff/generate_diff.py
+++ difference_calculator/gendiff/generate_diff.py
@@ -1,30 +1,30 @@
 import json
+from difference_calculator.parser.parser import parser
+
 
 PATH_TO_FILE1 = 'difference_calculator/file1.json'
 PATH_TO_FILE2 = 'difference_calculator/file2.json'
+PATH_TO_FILE1YMl = 'difference_calculator/file1.yml'
+PATH_TO_FILE2YMl = 'difference_calculator/file2.yml'
 
-
-def generate_diff(filename1, filename2):
-    fn1 = json.load(open(filename1))
-    fn2 = json.load(open(filename2))
+def gendiff(file1, file2):
     result = dict()
-    for i in sorted(fn1.keys()):
-        if i not in fn2:
-            result[f'- {i}'] = fn1[i]
-            del fn1[i]
-        elif fn1[i] == fn2.get(i):
-            result[f'  {i}'] = fn1[i]
-            del fn1[i], fn2[i]
-        elif i in fn2:
-            result[f'- {i}'] = fn1[i]
-            result[f'+ {i}'] = fn2[i]
-            del fn1[i], fn2[i]
-    for i in fn2:
-        result[f"+ {i}"] = fn2[i]
+    for i in sorted(file1.keys()):
+        if i not in file2:
+            result[f'- {i}'] = file1[i]
+            del file1[i]
+        elif file1[i] == file2.get(i):
+            result[f'  {i}'] = file1[i]
+            del file1[i], file2[i]
+        elif i in file2:
+            result[f'- {i}'] = file1[i]
+            result[f'+ {i}'] = file2[i]
+            del file1[i], file2[i]
+    for i in file2:
+        result[f"+ {i}"] = file2[i]
         del i
     print(json.dumps(result, indent=2))
     return json.dumps(result, indent=2)
-
-
 def main():
-    generate_diff(PATH_TO_FILE1, PATH_TO_FILE2)
+    parser('difference_calculator/file1.yml', 'difference_calculator/file2.yml')
+    parser('difference_calculator/file1.json', 'difference_calculator/file2.json')
---
+++ difference_calculator/parser/parser.py
@@ -0,0 +1,21 @@
+
+import yaml
+import json
+
+def parser(path_to_file1, path_to_file2):
+    from difference_calculator.gendiff.generate_diff import gendiff
+    if '.yml' in path_to_file1 or '.yaml' in path_to_file2:
+        print('yaml')
+        with open(path_to_file1) as file1:
+            with open(path_to_file2) as file2:
+                fn1 = yaml.safe_load(file1)
+                fn2 = yaml.safe_load(file2)
+        return gendiff(fn1, fn2)
+    print('json')
+    fn1 = json.load(open(path_to_file1))
+    fn2 = json.load(open(path_to_file2))
+    return gendiff(fn1, fn2)
+
+def main():
+    parser('difference_calculator/file1.yml', 'difference_calculator/file2.yml')
+    parser('difference_calculator/file1.json', 'difference_calculator/file2.json')
