1f3f3536e521b580e96db55109d383896c8aab92
NevermoreKatana <crymorebch@gmail.com>
2023-07-04 14:47:53 +0300

    Refactoring

--- difference_calculator/parser/parser.py
+++ difference_calculator/parser/parser.py
@@ -3,15 +3,12 @@ import json
 
 
 def pars(path_to_file1, path_to_file2):
-    if '.yml' in path_to_file1 or '.yaml' in path_to_file2:
-        with open(path_to_file1) as file1:
-            with open(path_to_file2) as file2:
-                data1 = yaml.safe_load(file1)
-                data2 = yaml.safe_load(file2)
-        return data1, data2
-
     with open(path_to_file1) as file1:
         with open(path_to_file2) as file2:
+            if '.yml' in path_to_file1 or '.yaml' in path_to_file2:
+                data1 = yaml.safe_load(file1)
+                data2 = yaml.safe_load(file2)
+                return data1, data2
             data1 = json.load(file1)
             data2 = json.load(file2)
-    return data1, data2
+        return data1, data2
