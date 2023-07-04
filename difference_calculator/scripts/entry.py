2e79c08a923f0de68340b2a1f2e4ecda2eda1f58
NevermoreKatana <crymorebch@gmail.com>
2023-07-04 12:54:31 +0300

    Add entry script

---
+++ difference_calculator/scripts/entry.py
@@ -0,0 +1,14 @@
+from difference_calculator.gendiff.generate_diff import generate_diff_dict
+from difference_calculator.parser.parser import pars
+from difference_calculator.formatting.plain_format import generate_diff_plain
+from difference_calculator.cmd_gendiff import cmd_gendiff
+
+
+def main():
+    PATH_TO_FILE1, PATH_TO_FILE2, FORMAT = cmd_gendiff()
+    data1, data2 = pars(PATH_TO_FILE1, PATH_TO_FILE2)
+    if FORMAT == 'plain':
+        generate_diff_dict(data1, data2)
+        generate_diff_plain(data1, data2)
+    else:
+        generate_diff_dict(data1, data2)
