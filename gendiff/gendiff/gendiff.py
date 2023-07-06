347de25ecc7d75f706eeeb9f6a57b96005ce1c18
NevermoreKatana <crymorebch@gmail.com>
2023-07-06 17:14:09 +0300

    update

--- gendiff/gendiff/gendiff.py
+++ gendiff/gendiff/gendiff.py
@@ -5,7 +5,7 @@ from gendiff.parser import cmd_gendiff
 from gendiff.renderers.json import generate_diff_dict_json
 
 
-def generate_diff():
+def generate_diff(PATH_TO_FILE1, PATH_TO_FILE2):
     PATH_TO_FILE1, PATH_TO_FILE2, FORMAT = cmd_gendiff()
     data1, data2 = file_type(PATH_TO_FILE1, PATH_TO_FILE2)
     if FORMAT == 'plain':
