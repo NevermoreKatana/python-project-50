01992d43a721cdf639af6fe893f52a47f6677dab
NevermoreKatana <crymorebch@gmail.com>
2023-07-09 13:04:47 +0300

    remove print

--- gendiff/renderers/standart.py
+++ gendiff/renderers/standart.py
@@ -45,7 +45,7 @@ def generate_diff(data1, data2):
 def format_diff(diff, indent=0):
     lines = []
     indent_str = ' ' * indent
-    nested_indent_str = ' ' * (indent + 4)
+    nested_indent_str = ' ' * (indent + 2)
     for key, item in diff.items():
         status = item['status']
         if status == 'added':
@@ -57,8 +57,8 @@ def format_diff(diff, indent=0):
             lines.append(f"{indent_str}+ {key}: {format_value(item['new_value'], indent)}")
         elif status == 'nested':
             lines.append(f"{indent_str}{key}:")
-            nested_diff = format_diff(item['children'], indent=indent + 4)
-            lines.append('\n'.join(nested_diff))
+            nested_diff = format_diff(item['children'], indent=indent + 2)
+            lines.extend(nested_diff)
         else:
             lines.append(f"{indent_str}  {key}: {format_value(item['value'], indent)}")
     return lines
@@ -67,9 +67,10 @@ def format_diff(diff, indent=0):
 def format_value(value, indent):
     if isinstance(value, dict):
         lines = []
+        nested_indent_str = ' ' * (indent + 2)
         for key, val in value.items():
-            lines.append(f"{indent * ' '}{key}: {val}")
-        return '{\n' + '\n'.join(lines) + '\n' + (indent - 2) * ' ' + '}'
+            lines.append(f"{nested_indent_str}{key}: {format_value(val, indent + 2)}")
+        return '{\n' + '\n'.join(lines) + '\n' + nested_indent_str + '}'
     else:
         return value
 
@@ -77,13 +78,17 @@ def format_value(value, indent):
 def generate_diff_dict(data1, data2):
     diff = generate_diff(data1, data2)
     formatted_diff = format_diff(diff)
-    formatted_diff_str = '{\n' + '\n'.join(formatted_diff) + '\n}'
-    # print(formatted_diff_str)
+    formatted_diff_str = '\n'.join(formatted_diff)
+    formatted_diff_str = '{\n' + formatted_diff_str + '\n}'
+    print(formatted_diff_str)
     return formatted_diff_str
 
 
 def main():
     data1, data2 = file_type(PATH_TO_FILE1_JSON, PATH_TO_FILE2_JSON)
     generate_diff_dict(data1, data2)
+
+
+
 
 
