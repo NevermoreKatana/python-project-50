2db3644d61b47ce7a6ceaed1db3408cc2928797d
KatanaNevermore <crymorebch@gmail.com>
2023-07-19 14:23:41 +0300

    remove argparser func

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,6 +1,6 @@
 import yaml
 import json
-import argparse
+
 
 
 def load_files(path_to_file1, path_to_file2):
@@ -15,13 +15,4 @@ def load_files(path_to_file1, path_to_file2):
         return data1, data2
 
 
-def cmd_gendiff():
-    parser = argparse.ArgumentParser(
-        description='Compares two configuration files and shows a difference.')
-    parser.add_argument("first_file", metavar='<file_path1>')
-    parser.add_argument("second_file", metavar='<file_path2>')
-    parser.add_argument('-f', '--format', help="set format of output",
-                        default=None)
 
-    args = parser.parse_args()
-    return args.first_file, args.second_file, args.format
