060347117f52e18fe13518cc72fcc1758b5bda57
KatanaNevermore <crymorebch@gmail.com>
2023-07-19 19:36:55 +0300

    add new tests

--- tests/test_json_format.py
+++ tests/test_json_format.py
@@ -1,6 +1,18 @@
-from gendiff.renderers.json import (
-    format_diff_json,
-)
+import json
+from gendiff.renderers.json import format_diff_json, generate_diff_json
+from gendiff.parser import load_files
+
+PATH_TO_FILE1_JSON = 'example_files/file1.json'
+PATH_TO_FILE2_JSON = 'example_files/file2.json'
+
+PATH_TO_FILE1_YML = 'example_files/file1.yml'
+PATH_TO_FILE2_YML = 'example_files/file2.yml'
+
+PATH_TO_TEST_FILE_JSON = 'tests/fixtures/test_json.json'
+PATH_TO_TEST_FAILURE_FILE_JSON = 'tests/fixtures/test_failure_json.json'
+
+PATH_TO_TEST_FILE_YML = 'tests/fixtures/test_yml.yml'
+PATH_TO_TEST_FAILURE_FILE_YML = 'tests/fixtures/test_failure_yaml.yaml'
 
 
 def test_format_diff_json():
@@ -21,3 +33,37 @@ def test_format_diff_json():
     diff_tree = []
     expected = {}
     assert format_diff_json(diff_tree) == expected
+
+
+def test_json_format_json():
+    with open(PATH_TO_TEST_FILE_JSON, 'r') as file:
+        expected = json.load(file)
+    data1, data2 = load_files(PATH_TO_FILE1_JSON, PATH_TO_FILE2_JSON)
+
+    assert generate_diff_json(data1, data2) == expected
+
+
+def test_json_format_yml():
+    with open(PATH_TO_TEST_FILE_YML, 'r') as file:
+        expected = json.load(file)
+
+    data1, data2 = load_files(PATH_TO_FILE1_JSON, PATH_TO_FILE2_JSON)
+
+    assert generate_diff_json(data1, data2) == expected
+
+
+def test__failure_json_format_json():
+    with open(PATH_TO_TEST_FAILURE_FILE_JSON, 'r') as file:
+        expected = json.load(file)
+    data1, data2 = load_files(PATH_TO_FILE1_JSON, PATH_TO_FILE2_JSON)
+
+    assert generate_diff_json(data1, data2) != expected
+
+
+def test_failure_json_format_yml():
+    with open(PATH_TO_TEST_FAILURE_FILE_YML, 'r') as file:
+        expected = json.load(file)
+
+    data1, data2 = load_files(PATH_TO_FILE1_JSON, PATH_TO_FILE2_JSON)
+
+    assert generate_diff_json(data1, data2) != expected
