1c1ac7aea8f3490d668ae10a3c563c9224f8f16c
KatanaNevermore <crymorebch@gmail.com>
2023-07-19 14:23:32 +0300

    add argparser file

---
+++ gendiff/cli.py
@@ -0,0 +1,12 @@
+import argparse
+
+def cmd_gendiff():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.')
+    parser.add_argument("first_file", metavar='<file_path1>')
+    parser.add_argument("second_file", metavar='<file_path2>')
+    parser.add_argument('-f', '--format', help="set format of output",
+                        default=None)
+
+    args = parser.parse_args()
+    return args.first_file, args.second_file, args.format
