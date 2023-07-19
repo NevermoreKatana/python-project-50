931ed60207534346ea8e2fb6daa0f0c73f64d337
KatanaNevermore <crymorebch@gmail.com>
2023-07-19 13:01:18 +0300

    update

--- gendiff/renderers/stylish.py
+++ gendiff/renderers/stylish.py
@@ -1,45 +1,24 @@
 import json
 from gendiff.parser import load_files
 from gendiff.renderers.diff_finder import find_diff
-
-
 def generate_diff_stylish(data1, data2):
     diff_tree = find_diff(data1, data2)
     diff = format_diff_stylish(diff_tree)
     return diff
-
-
-def format_diff_stylish(diff_tree, indent="    "):
+def format_diff_stylish(diff_tree, indent="  "):
     lines = []
     lines.append("{")  # Добавление начальной открывающей скобки
     lines.extend(format_diff_items(diff_tree, indent))
-    lines.append("}")  # Добавление конечной закрывающей скобки
+    lines.append(f"{indent}}}")  # Добавление конечной закрывающей скобки с учетом отступа
     return "\n".join(lines)
 
 
-def format_diff_stylish(diff_tree, indent="    "):
-    lines = []
-    lines.append("{")  # Добавление начальной открывающей скобки
-    lines.extend(format_diff_items(diff_tree, indent))
-    lines.append(indent + "}")  # Добавление конечной закрывающей скобки с учетом отступа
-    return "\n".join(lines)
-
-
-def format_diff_stylish(diff_tree, indent="    "):
-    lines = []
-    lines.append("{")  # Добавление начальной открывающей скобки
-    lines.extend(format_diff_items(diff_tree, indent))
-    lines.append(indent + "}")  # Добавление конечной закрывающей скобки с учетом отступа
-    return "\n".join(lines)
-
-
-def format_diff_items(diff_tree, indent="    "):
+def format_diff_items(diff_tree, indent="  "):
     lines = []
     for item in diff_tree:
         node_type = item['type']
         key = item['key']
         value = item.get('value')
-
         if node_type == 'nested':
             nested_diff = format_diff_items(item['children'], indent + '    ')
             lines.append(f"{indent}  {key}: {{")
@@ -59,18 +38,15 @@ def format_diff_items(diff_tree, indent="    "):
         elif node_type == 'unchanged':
             value = format_value(value, indent + '    ')
             lines.append(f"{indent}  {key}: {value}")
-
     return lines
 
-
-def format_value(value, indent="    "):
+def format_value(value, indent="  "):
     if isinstance(value, dict):
         lines = [f"{indent}    {k}: {format_value(v, indent + '    ')}"
                  for k, v in value.items()]
-        return "{\n" + "\n".join(lines) + f"\n{indent}    }}"  # Добавление закрывающей скобки с учетом отступа
+        return "{\n" + "\n".join(lines) + f"\n{indent}  }}"
     else:
         return json.dumps(value, ensure_ascii=False).strip('"')
-
 def main():
     PATH_TO_FILE1_JSON = "example_files/file1.yml"
     PATH_TO_FILE2_JSON = "example_files/file2.yml"
