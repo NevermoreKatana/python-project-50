e18963aeb1f7ec69375f5fa5705f35a183b3351a
KatanaNevermore <crymorebch@gmail.com>
2023-07-19 12:48:13 +0300

    update

--- gendiff/renderers/stylish.py
+++ gendiff/renderers/stylish.py
@@ -9,15 +9,23 @@ def generate_diff_stylish(data1, data2):
     return diff
 
 
-def format_diff_stylish(diff_tree, indent="  "):
+def format_diff_stylish(diff_tree, indent="    "):
     lines = []
     lines.append("{")  # Добавление начальной открывающей скобки
     lines.extend(format_diff_items(diff_tree, indent))
-    lines.append(f"{indent}}}")  # Добавление конечной закрывающей скобки с учетом отступа
+    lines.append("}")  # Добавление конечной закрывающей скобки
     return "\n".join(lines)
 
 
-def format_diff_items(diff_tree, indent="  "):
+def format_diff_stylish(diff_tree, indent="    "):
+    lines = []
+    lines.append("{")  # Добавление начальной открывающей скобки
+    lines.extend(format_diff_items(diff_tree, indent))
+    lines.append(indent + "}")  # Добавление конечной закрывающей скобки с учетом отступа
+    return "\n".join(lines)
+
+
+def format_diff_items(diff_tree, indent="    "):
     lines = []
     for item in diff_tree:
         node_type = item['type']
@@ -25,35 +33,39 @@ def format_diff_items(diff_tree, indent="  "):
         value = item.get('value')
 
         if node_type == 'nested':
-            nested_diff = format_diff_items(item['children'], indent + '    ')
+            nested_diff = format_diff_items(item['children'], indent + '  ')
             lines.append(f"{indent}  {key}: {{")
             lines.extend(nested_diff)
             lines.append(f"{indent}  }}")
         elif node_type == 'added':
-            value = format_value(value, indent + '    ')
+            value = format_value(value, indent + '  ')
             lines.append(f"{indent}+ {key}: {value}")
         elif node_type == 'removed':
             value = format_value(value, indent + '    ')
             lines.append(f"{indent}- {key}: {value}")
         elif node_type == 'changed':
-            old_value = format_value(item['old_value'], indent + '    ')
-            new_value = format_value(item['new_value'], indent + '    ')
+            old_value = format_value(item['old_value'], indent + '  ')
+            new_value = format_value(item['new_value'], indent + '  ')
             lines.append(f"{indent}- {key}: {old_value}")
             lines.append(f"{indent}+ {key}: {new_value}")
         elif node_type == 'unchanged':
-            value = format_value(value, indent + '    ')
+            value = format_value(value, indent + '  ')
             lines.append(f"{indent}  {key}: {value}")
 
     return lines
 
 
-def format_value(value, indent="  "):
+def format_value(value, indent="    "):
     if isinstance(value, dict):
         lines = [f"{indent}    {k}: {format_value(v, indent + '    ')}"
                  for k, v in value.items()]
-        return "{\n" + "\n".join(lines) + f"\n{indent}  }}"
+        return "{\n" + "\n".join(lines) + f"\n{indent}}}"  # Добавление закрывающей скобки с учетом отступа
     else:
         return json.dumps(value, ensure_ascii=False).strip('"')
+
+
+# Остальной код остается неизменным
+
 
 
 def main():
