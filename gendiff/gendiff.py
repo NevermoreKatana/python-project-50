212ada9227538de4c36d8fa43a67a12edb6a6ef0
KatanaNevermore <crymorebch@gmail.com>
2023-07-19 14:17:42 +0300

    update generate_diff func logic

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,23 +1,29 @@
-from gendiff.renderers.stylish import generate_diff_stylish
-from gendiff.parser import load_files
-from gendiff.renderers.plain import generate_diff_plain
 from gendiff.parser import cmd_gendiff
-from gendiff.renderers.json import generate_diff_dict_json
+from gendiff.parser import load_files
+from gendiff.renderers.plain import gendiff_plain
+from gendiff.renderers.json import gendiff_json
+from gendiff.renderers.stylish import generate_diff_stylish
+
 
 
 def main():
     PATH_TO_FILE1, PATH_TO_FILE2, FORMAT = cmd_gendiff()
     data1, data2 = load_files(PATH_TO_FILE1, PATH_TO_FILE2)
     if FORMAT == 'plain':
-        generate_diff_plain(data1, data2)
+        gendiff_plain(data1, data2)
     elif FORMAT == 'json':
-        generate_diff_dict_json(data1, data2)
+        gendiff_json(data1, data2)
     else:
         d = generate_diff_stylish(data1, data2)
         print(d)
 
 
-def generate_diff(PATH_TO_FILE1, PATH_TO_FILE2):
+def generate_diff(PATH_TO_FILE1, PATH_TO_FILE2, style='stylish'):
     data1, data2 = load_files(PATH_TO_FILE1, PATH_TO_FILE2)
-    diff = generate_diff_stylish(data1, data2)
+    if style == 'stylish':
+        diff = generate_diff_stylish(data1, data2)
+    if style == 'plain':
+        diff = gendiff_plain(data1, data2)
+    else:
+        diff = gendiff_json(data1, data2)
     return diff
