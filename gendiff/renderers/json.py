bdfd9238fa2a7fb7b402f129cab4e4f3ba0d2cfe
NevermoreKatana <grenkachannel@hotmail.com>
2023-07-25 19:17:31 +0300

    Update

--- gendiff/renderers/json.py
+++ gendiff/renderers/json.py
@@ -2,37 +2,7 @@ import json
 from gendiff.diff_finder import find_diff
 
 
-def compare_values(value1, value2):
-    if isinstance(value1, dict) and isinstance(value2, dict):
-        return generate_diff_json(value1, value2)
-    elif value1 == value2:
-        return {'status': 'unchanged', 'value': value1}
-    elif value1 != value2:
-        return {'status': 'changed', 'old_value': value1, 'new_value': value2}
-
-
-def format_diff_json(diff_tree):
-    formatted_diff = {}
-    for item in diff_tree:
-        node_type = item['type']
-        key = item['key']
-
-        if node_type == 'nested':
-            formatted_diff[key] = format_diff_json(item['children'])
-        elif node_type == 'added':
-            formatted_diff[key] = item['value']
-        elif node_type == 'removed':
-            formatted_diff[key] = item['value']
-        elif node_type == 'changed':
-            formatted_diff[key] = item['new_value']
-        else:
-            formatted_diff[key] = item['value']
-
-    return formatted_diff
-
-
 def generate_diff_json(data1, data2):
     diff_tree = find_diff(data1, data2)
-    diff = format_diff_json(diff_tree)
-    diff = json.dumps(diff, indent=2)
+    diff = json.dumps(diff_tree, indent=2)
     return diff
--- tests/test_json_format.py
+++ tests/test_json_format.py
@@ -1,5 +1,5 @@
 import json
-from gendiff.renderers.json import format_diff_json, generate_diff_json
+from gendiff.renderers.json import generate_diff_json
 from gendiff.parser import load_files
 
 PATH_TO_FILE1_JSON = 'example_files/file1.json'
@@ -13,46 +13,6 @@ PATH_TO_TEST_FAILURE_FILE_JSON = 'tests/fixtures/test_failure_json.json'
 
 PATH_TO_TEST_FILE_YML = 'tests/fixtures/test_yml.yml'
 PATH_TO_TEST_FAILURE_FILE_YML = 'tests/fixtures/test_failure_yaml.yaml'
-
-
-def test_format_diff_json():
-    diff_tree = [
-        {'type': 'added', 'key': 'a', 'value': 1},
-        {'type': 'removed', 'key': 'b', 'value': 2},
-        {'type': 'changed', 'key': 'c', 'new_value': 3},
-        {'type': 'unchanged', 'key': 'd', 'value': 4}
-    ]
-    expected = {
-        'a': 1,
-        'b': 2,
-        'c': 3,
-        'd': 4
-    }
-    assert format_diff_json(diff_tree) == expected
-
-    diff_tree = []
-    expected = {}
-    assert format_diff_json(diff_tree) == expected
-
-
-def test_json_format_json():
-    with open(PATH_TO_TEST_FILE_JSON, 'r') as file:
-        expected = json.load(file)
-        expected = json.dumps(expected, indent=2)
-
-    data1, data2 = load_files(PATH_TO_FILE1_JSON, PATH_TO_FILE2_JSON)
-
-    assert generate_diff_json(data1, data2) == expected
-
-
-def test_json_format_yml():
-    with open(PATH_TO_TEST_FILE_YML, 'r') as file:
-        expected = json.load(file)
-        expected = json.dumps(expected, indent=2)
-
-    data1, data2 = load_files(PATH_TO_FILE1_JSON, PATH_TO_FILE2_JSON)
-
-    assert generate_diff_json(data1, data2) == expected
 
 
 def test__failure_json_format_json():
