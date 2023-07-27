ec55e1ff52ee839081fbc5c0f0fc53706cff4dc7
NevermoreKatana <grenkachannel@hotmail.com>
2023-07-27 19:18:15 +0300

    splitting a function into parts

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -2,13 +2,21 @@ import yaml
 import json
 
 
+def read_json_file(path_to_file):
+    with open(path_to_file, 'r') as file:
+        return json.load(file)
+
+
+def read_yml_file(path_to_file):
+    with open(path_to_file, 'r') as file:
+        return yaml.safe_load(file)
+
+
 def load_files(path_to_file1, path_to_file2):
-    with open(path_to_file1) as file1:
-        with open(path_to_file2) as file2:
-            if '.yml' in path_to_file1 or '.yaml' in path_to_file2:
-                data1 = yaml.safe_load(file1)
-                data2 = yaml.safe_load(file2)
-                return data1, data2
-            data1 = json.load(file1)
-            data2 = json.load(file2)
-        return data1, data2
+    if '.yml' in path_to_file1 or '.yaml' in path_to_file2:
+        data1 = read_yml_file(path_to_file1)
+        data2 = read_yml_file(path_to_file2)
+    else:
+        data1 = read_json_file(path_to_file1)
+        data2 = read_json_file(path_to_file2)
+    return data1, data2
